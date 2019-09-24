5fbd3d7321493f66ef6eccea39465d4267185838
Abhijit <ababhijit70@yahoo.com>
2019-09-24 06:06:10 +0530

    Enable Django Admin for user profile

--- profile_api/models.py
+++ profile_api/models.py
@@ -1,26 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
+
 class UserProfileManager(BaseUserManager):
     """Manager for user profile """
 
-    def create_user(self,email,name,password=None):
+    def create_user(self, email, name, password=None):
         """Create a new user profile """
         if not email :
             raise ValueError('User must have an email address')
 
-        email = self.normalize_email (email)
+        email = self.normalize_email(email)
         user = self.model(email=email,name=name)
-        user.set_password(pasword)
+        user.set_password(password)
         user.save(using=self._db)
 
         return user
 
     def create_superuser(self,email,name,password):
         """Create and save a new super uer with given details"""
-        user = create_user(email,name,password)
+        user = self.create_user(email,name,password)
         user.is_superuser = True
         user.is_staff =True
         user.save(using=self._db)
@@ -29,20 +31,21 @@ class UserProfileManager(BaseUserManager):
 
 
 
-class UserProfile (AbstractBaseUser,PermissionsMixin):
+class UserProfile(AbstractBaseUser,PermissionsMixin):
     """ Database model for users in the system"""
     email = models.EmailField(max_length=255,unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
+    # custom manager
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrieve ful name of user"""
+        """Retrieve full name of user"""
         return self.name
     def get_sort_name(self):
         """Retrieve sort name of user """
